dd0fb043fbe70d82307c9a35c40a35fa12023d86
Venneg <Venneg7205@gmail.com>
2024-05-16 11:18:40 +0200

    Добавьте файлы проекта.

---
+++ Project-API-Docker-Linux/Models/ОтделКадров.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_API_Docker_Linux.Models;
+
+public partial class ОтделКадров
+{
+    public string? Фио { get; set; }
+
+    public string? Наименование { get; set; }
+}
